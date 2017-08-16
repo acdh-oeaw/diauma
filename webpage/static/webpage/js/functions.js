b478c25000f242812380260843fe16515a1928a2
Alexander Watzinger <alexander.watzinger@craws.net>
2017-08-16 16:57:52 +0200

    added js for jstree and cookieconsent

--- webpage/static/webpage/js/functions.js
+++ webpage/static/webpage/js/functions.js
@@ -16,6 +16,46 @@ $(document).ready(function () {
     });
 })
 
+
+function createTreeOverlay(name, title, multiple=false) {
+    $("#" + name + "Overlay").click(function () {
+        $("#" + name + "Dialog").dialog("close");
+    });
+    $("#" + name + "Button").click(function () {
+        $("#" + name + "Overlay").height($(window).height());
+        $("#" + name + "Overlay").width($(window).width());
+        $("#" + name + "Overlay").fadeTo(1, 0.6);
+        $("#" + name + "Dialog").dialog({
+            position: {my: "center top", at: "center top+80", of: window},
+            closeText: "X",
+            title: title,
+            closeOnEscape: true,
+            width: "auto",
+            height: "auto",
+            close: function () {
+                if (multiple) {
+                    selectFromTreeMulti(name);
+                }
+                $("#" + name + "Overlay").css("display", "none");
+            }
+        });
+    });
+}
+
+function selectFromTreeMulti(name) {
+    var ids = $('#' + name + 'Tree').jstree('get_selected');
+    var checkedNames = '';
+    ids.forEach(function (item, index, array) {
+        var node = $('#' + name + 'Tree').jstree().get_node(item);
+        checkedNames += node['text'] + "<br/>";
+    });
+    $("#" + name + "Id").val(ids);
+    $("#" + name + "Selection").html(checkedNames);
+}
+
+
+
+
 /* Piwik */
 var _paq = _paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
@@ -23,10 +63,10 @@ _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
     var u="//clarin.oeaw.ac.at/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setTrackerUrl', u+'piwik/piwik.php']);
     _paq.push(['setSiteId', '64']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik/piwik.js'; s.parentNode.insertBefore(g,s);
 })();
 
 /* cookie consent */
