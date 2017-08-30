a29d79a2e40bb8f847818db1c11cb7d6ec9541b3
Alexander Watzinger <alexander.watzinger@craws.net>
2017-08-30 13:25:00 +0200

    raised version

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -3,7 +3,7 @@
 
 PROJECT_METADATA = {
     'title': 'Diauma',
-    'version': '0.2.0',
+    'version': '0.3.0',
     'piwik_id': '64',
     'author': 'Alexander Watzinger',
     'subtitle': 'Austrian Dialect Cartography 1924–1956. Digitalisation, Contextualisation, Visualisation',
