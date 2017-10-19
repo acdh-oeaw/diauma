06b122163f5db32ce5946e29932f16c7ecfb2074
Alexander Watzinger <alexander.watzinger@craws.net>
2017-10-19 17:21:19 +0200

    Raised scan file limit

--- diauma/settings/base.py
+++ diauma/settings/base.py
@@ -9,7 +9,7 @@ ALLOWED_UPLOAD_EXTENSIONS = ['jp2', 'jpeg', 'jpg', 'pdf', 'png', 'tif', 'tiff', 
 ALLOWED_UPLOAD_SIZE = 20 * 1024 * 1024  # in bytes, e.g for 2 MB: 2 * 1024 * 1024
 
 ALLOWED_SCAN_EXTENSIONS = ['jp2', 'tif', 'tiff']
-ALLOWED_SCAN_SIZE = 200 * 1024 * 1024  # in bytes, e.g for 2 MB: 2 * 1024 * 1024
+ALLOWED_SCAN_SIZE = 10 * 1024 * 1024 * 1024  # in bytes, e.g for 2 MB: 2 * 1024 * 1024
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
