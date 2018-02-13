51c3ebeaf908a1449efa201fbd732fe7f3d452c5
Alexander Watzinger <alexander.watzinger@craws.net>
2018-02-13 11:49:29 +0100

    Added project purpose in english and german to imprint.

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -9,5 +9,5 @@ PROJECT_METADATA = {
     'subtitle': 'Austrian Dialect Cartography 1924–1956. Digitalisation, Contextualisation, Visualisation',
     'description': '',
     'github': 'https://github.com/acdh-oeaw/diauma',
-    'purpose_de': '',
-    'purpose_en': ''}
+    'purpose_de': 'Dieses Projekt ist fokusiert auf die österreichischen Dialektkartographie von 1924 bis 1956.',
+    'purpose_en': 'The project is focusing on Austrian dialect cartography from 1924 to 1956.'}
