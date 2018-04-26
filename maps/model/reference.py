e8e22913fbb725bd6971b5741d8145af4e37c9b2
Alexander Watzinger <alexander.watzinger@craws.net>
2018-04-26 15:53:05 +0200

    Added super for references in model

--- maps/model/reference.py
+++ maps/model/reference.py
@@ -1,5 +1,5 @@
 # Created by Alexander Watzinger at the ACDH. Please see README.md for licensing information
-from django.db.models import (CharField, ManyToManyField, TextField)
+from django.db.models import (CASCADE, CharField, ManyToManyField, TextField, ForeignKey)
 from django.utils.translation import ugettext_lazy
 
 from maps.model.base import BaseModel
@@ -11,6 +11,8 @@ class Reference(BaseModel):
     info = TextField(blank=True)
     reference_type = ManyToManyField(Type, blank=True, related_name='reference_type',
                                      verbose_name=ugettext_lazy('types'))
+    super = ForeignKey('self', blank=True, null=True, related_name='sub',
+                          verbose_name=ugettext_lazy('has super'), on_delete=CASCADE)
 
     def __str__(self):
         return self.name
