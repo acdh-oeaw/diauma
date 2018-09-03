f1b793ee9c00b3b447cecc62d6704d6c859b0ce7
Alexander Watzinger <alexander.watzinger@craws.net>
2018-09-03 12:45:15 +0200

    Removed broken table pagination from search

--- maps/views/search.py
+++ maps/views/search.py
@@ -1,5 +1,4 @@
 # Created by Alexander Watzinger at the ACDH. Please see README.md for licensing information
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -20,27 +19,22 @@ def index(request):
     maps = Map.objects.filter(name__icontains=search_term)
     if maps:
         table = MapTable(maps)
-        table.paginate(page=request.GET.get('page', 1), per_page=settings.TABLE_ITEMS_PER_PAGE)
         result_tables['Maps'] = table
     references = Reference.objects.filter(name__icontains=search_term)
     if references:
         table = ReferenceTable(references)
-        table.paginate(page=request.GET.get('page', 1), per_page=settings.TABLE_ITEMS_PER_PAGE)
         result_tables['References'] = table
     persons = Person.objects.filter(name__icontains=search_term)
     if persons:
         table = PersonTable(persons)
-        table.paginate(page=request.GET.get('page', 1), per_page=settings.TABLE_ITEMS_PER_PAGE)
         result_tables['Persons'] = table
     institutes = Institute.objects.filter(name__icontains=search_term)
     if institutes:
         table = InstituteTable(institutes)
-        table.paginate(page=request.GET.get('page', 1), per_page=settings.TABLE_ITEMS_PER_PAGE)
         result_tables['Institutes'] = table
     places = Place.objects.filter(name__icontains=search_term)
     if places:
         table = PlaceTable(places)
-        table.paginate(page=request.GET.get('page', 1), per_page=settings.TABLE_ITEMS_PER_PAGE)
         result_tables['Places'] = table
     return render(
         request,
