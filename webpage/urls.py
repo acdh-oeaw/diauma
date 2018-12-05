2893a094fad44ce06c0b0e43e67ac19c36577941
Alexander Watzinger <alexander.watzinger@craws.net>
2018-12-05 17:11:08 +0100

    Adaptions after making Django updates

--- webpage/urls.py
+++ webpage/urls.py
@@ -7,9 +7,8 @@ from django.views.static import serve
 
 from . import views
 
-favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
-
 app_name = "webpage"
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     url(r'^favicon\.ico$', favicon_view),
@@ -18,6 +17,5 @@ urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^imprint/$', TemplateView.as_view(template_name='webpage/imprint.html'), name="imprint"),
     url(r'^logout/$', views.user_logout, name='logout'),
-    url(r'^login/$', auth_views.login, {'template_name': 'webpage/login.html'}, name='login'),
-    url(r'^docs/(?P<path>.*)', serve, {'document_root': 'docs/html'}, 'docs'),
-]
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='webpage/login.html'), name='login'),
+    url(r'^docs/(?P<path>.*)', serve, {'document_root': 'docs/html'}, 'docs')]
