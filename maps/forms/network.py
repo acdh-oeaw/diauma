85e9ebced15d87caefb05f82b6da2821828ac05e
Alexander Watzinger <alexander.watzinger@craws.net>
2019-04-18 11:05:48 +0200

    Implemented huebee color picker for network graph

--- maps/forms/network.py
+++ maps/forms/network.py
@@ -3,7 +3,19 @@ from django.utils.translation import ugettext
 
 
 class NetworkForm(forms.Form):
-    color_map = forms.CharField(initial='#34b522', label=ugettext('person').capitalize())
-    color_person = forms.CharField(initial='#ffa500', label=ugettext('institute').capitalize())
-    color_institute = forms.CharField(initial='#ee82ee', label=ugettext('reference').capitalize())
-    color_reference = forms.CharField(initial='#34623c', label=ugettext('person').capitalize())
+    color_map = forms.CharField(
+        initial='#34b522',
+        label=ugettext('person').capitalize(),
+        widget=forms.TextInput(attrs={'data-huebee': True, 'class': 'data-huebee'}))
+    color_person = forms.CharField(
+        initial='#ffa500',
+        label=ugettext('institute').capitalize(),
+        widget=forms.TextInput(attrs={'data-huebee': True, 'class': 'data-huebee'}))
+    color_institute = forms.CharField(
+        initial='#ee82ee',
+        label=ugettext('reference').capitalize(),
+        widget=forms.TextInput(attrs={'data-huebee': True, 'class': 'data-huebee'}))
+    color_reference = forms.CharField(
+        initial='#34623c',
+        label=ugettext('person').capitalize(),
+        widget=forms.TextInput(attrs={'data-huebee': True, 'class': 'data-huebee'}))
