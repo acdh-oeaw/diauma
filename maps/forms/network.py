bbd5c1e317469adf1bd462b40595ea2c4515cc43
Alexander Watzinger <alexander.watzinger@craws.net>
2019-05-07 14:02:15 +0200

    New colors for map

--- maps/forms/network.py
+++ maps/forms/network.py
@@ -4,18 +4,18 @@ from django.utils.translation import ugettext
 
 class NetworkForm(forms.Form):
     color_map = forms.CharField(
-        initial='#34b522',
-        label=ugettext('person').capitalize(),
+        initial='#FF3030',
+        label=ugettext('map').capitalize(),
         widget=forms.TextInput(attrs={'data-huebee': True, 'class': 'data-huebee'}))
     color_person = forms.CharField(
-        initial='#ffa500',
-        label=ugettext('institute').capitalize(),
+        initial='#32CD32',
+        label=ugettext('person').capitalize(),
         widget=forms.TextInput(attrs={'data-huebee': True, 'class': 'data-huebee'}))
     color_institute = forms.CharField(
-        initial='#ee82ee',
-        label=ugettext('reference').capitalize(),
+        initial='#FFA500',
+        label=ugettext('institute').capitalize(),
         widget=forms.TextInput(attrs={'data-huebee': True, 'class': 'data-huebee'}))
     color_reference = forms.CharField(
-        initial='#34623c',
-        label=ugettext('person').capitalize(),
+        initial='#4169E1',
+        label=ugettext('reference').capitalize(),
         widget=forms.TextInput(attrs={'data-huebee': True, 'class': 'data-huebee'}))
