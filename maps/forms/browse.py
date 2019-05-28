207597c3e6edcf4a46ec24fbeacc30c947a0f8fc
Alexander Watzinger <alexander.watzinger@craws.net>
2019-05-28 12:27:53 +0200

    Translations

--- maps/forms/browse.py
+++ maps/forms/browse.py
@@ -1,13 +1,13 @@
 # Created by Alexander Watzinger at the ACDH. Please see README.md for licensing information
 from django import forms
-from django.utils.translation import ugettext
+from django.utils.translation import ugettext_lazy
 
 from maps.model.map import Map
 from maps.model.person import Person
 
 
 class BrowseForm(forms.Form):
-    choices = ((0, ugettext('all').capitalize()),)
+    choices = ((0, ugettext_lazy('All')),)
     for person in Person.objects.all():
         if Map.objects.filter(map_persons=person):
             choices += ((person.id, person.name),)
