00ee9ba7ef6bd90e2a203c0bc0d726396dae276f
Alexander Watzinger <alexander.watzinger@craws.net>
2019-08-13 16:51:26 +0200

    Overlay maps

--- maps/views/leaflet.py
+++ maps/views/leaflet.py
@@ -4,4 +4,14 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'maps/leaflet/index.html')
+    overlay_maps = {
+        0: {'name': 'Suggestiv Karte von Kärnten',
+            'file': 'suggestiv_kaernten.png',
+            'mapView': '[46.5, 14], 8',
+            'imageBounds': '[45.65171024913483,12.437753704352426],[47.29437290743704,15.67192558457878]'},
+        1: {'name': 'Suggestiv Karte von Kärnten',
+            'file': 'suggestiv_kaernten.png',
+            'mapView': '[46.5, 14], 8',
+            'imageBounds': '[45.65171024913483,12.437753704352426],[47.29437290743704,15.67192558457878]'}
+    }
+    return render(request, 'maps/leaflet/index.html', {'overlay_maps': overlay_maps})
