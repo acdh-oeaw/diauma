0369299a31183f8123c1922f8988efbe4f84c02d
Alexander Watzinger <alexander.watzinger@craws.net>
2019-08-27 15:17:30 +0200

    Show small images for georeference index site

--- maps/views/leaflet.py
+++ maps/views/leaflet.py
@@ -4,15 +4,15 @@ from django.shortcuts import render
 
 overlay_maps = {
     0: {'name': 'Suggestiv Karte von Kärnten',
-        'file': 'suggestiv_kaernten.png',
+        'file': 'suggestiv_kaernten',
         'map_view': '[46.5, 14], 8',
         'image_bounds': '[[45.65171024913483,12.437753704352426],[47.29437290743704,15.67192558457878]]'},
     1: {'name': 'Base map 4',
-        'file': 'base_map_4.png',
+        'file': 'base_map_4',
         'map_view': '[47.7, 14], 6',
         'image_bounds': '[[43.6601720195,8.73240499166],[51.678343824,19.298092196]]'},
     2: {'name': 'Base map 8',
-        'file': 'base_map_8.png',
+        'file': 'base_map_8',
         'map_view': '[47.5, 12], 6',
         'image_bounds': '[[43.3465552934,6.22248754564],[52.0009386071,19.737694157]]'}}
 
