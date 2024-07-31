3f878bf3b2d5cf883f70089f1f1758940d8128f6
csae8092 <peter.andorfer@oeaw.ac.at>
2024-07-31 15:51:18 +0200

    some linting

--- maps/views/files.py
+++ maps/views/files.py
@@ -30,135 +30,193 @@ from maps.util import get_selected_nodes
 def index(request):
     file_objects = File.objects.all()
     scan_objects = Scan.objects.all()
-    tables = {'files': FileTable(file_objects), 'scans': ScanTable(scan_objects)}
-    tables['files'].tab = '#tab-file'
-    tables['scans'].tab = '#tab-scan'
+    tables = {"files": FileTable(file_objects), "scans": ScanTable(scan_objects)}
+    tables["files"].tab = "#tab-file"
+    tables["scans"].tab = "#tab-scan"
     orphan_data = []
     # Get objects whose files are missing
     for file in file_objects:
         if not default_storage.exists(str(file.file)):
-            url_ = reverse('maps:file-detail', kwargs={'pk': file.pk})
-            orphan_data.append({'type': 'Missing file',
-                                'name': file,
-                                'source': mark_safe('<a href="' + url_ + '">Link</a>')})
+            url_ = reverse("maps:file-detail", kwargs={"pk": file.pk})
+            orphan_data.append(
+                {
+                    "type": "Missing file",
+                    "name": file,
+                    "source": mark_safe('<a href="' + url_ + '">Link</a>'),
+                }
+            )
     for scan in scan_objects:
         if not default_storage.exists(str(scan.file)):
-            url_ = reverse('maps:scan-detail', kwargs={'pk': scan.pk})
-            orphan_data.append({'type': 'Missing scan',
-                                'name': scan,
-                                'source': mark_safe('<a href="' + url_ + '">Link</a>')})
+            url_ = reverse("maps:scan-detail", kwargs={"pk": scan.pk})
+            orphan_data.append(
+                {
+                    "type": "Missing scan",
+                    "name": scan,
+                    "source": mark_safe('<a href="' + url_ + '">Link</a>'),
+                }
+            )
     # Get orphaned files
     orphaned_files_count = 0
-    path = settings.MEDIA_ROOT + 'scan/'
+    path = settings.MEDIA_ROOT + "scan/"
     scans = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     for file in scans:
-        if not Scan.objects.filter(file='scan/' + file):
+        if not Scan.objects.filter(file="scan/" + file):
             orphaned_files_count += 1
-            link = '<a class="button" download target="_blank" href="/media/scan/' + file + '">'
-            link += 'Download</a>'
-            orphan_data.append({'type': 'Orphaned scan',
-                                'name': file,
-                                'size': filesizeformat(os.path.getsize(path + file)),
-                                'source': mark_safe(link)})
-    path = settings.MEDIA_ROOT + 'file/'
+            link = (
+                '<a class="button" download target="_blank" href="/media/scan/'
+                + file
+                + '">'
+            )
+            link += "Download</a>"
+            orphan_data.append(
+                {
+                    "type": "Orphaned scan",
+                    "name": file,
+                    "size": filesizeformat(os.path.getsize(path + file)),
+                    "source": mark_safe(link),
+                }
+            )
+    path = settings.MEDIA_ROOT + "file/"
     files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     for file in files:
-        if not File.objects.filter(file='file/' + file):
+        if not File.objects.filter(file="file/" + file):
             orphaned_files_count += 1
-            link = '<a class="button" download target="_blank" href="/media/file/' + file + '">'
-            link += 'Download</a>'
-            orphan_data.append({'type': 'Orphaned file',
-                                'name': file,
-                                'size': filesizeformat(os.path.getsize(path + file)),
-                                'source': mark_safe(link)})
-    path = settings.MEDIA_ROOT + 'IIIF/'
+            link = (
+                '<a class="button" download target="_blank" href="/media/file/'
+                + file
+                + '">'
+            )
+            link += "Download</a>"
+            orphan_data.append(
+                {
+                    "type": "Orphaned file",
+                    "name": file,
+                    "size": filesizeformat(os.path.getsize(path + file)),
+                    "source": mark_safe(link),
+                }
+            )
+    path = settings.MEDIA_ROOT + "IIIF/"
     files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     for file in files:
         file_name = splitext(basename(file))[0]  # basename without extension
-        if not Scan.objects.filter(file__contains='scan/' + file_name):
+        if not Scan.objects.filter(file__contains="scan/" + file_name):
             orphaned_files_count += 1
-            link = '<a class="button" download target="_blank" href="/media/IIIF/' + file + '">'
-            link += 'Download</a>'
-            orphan_data.append({'type': 'Orphaned IIIF file',
-                                'name': file,
-                                'size': filesizeformat(os.path.getsize(path + file)),
-                                'source': mark_safe(link)})
-    tables['orphans'] = OrphanTable(orphan_data)
-    tables['orphans'].tab = '#tab-orphans'
+            link = (
+                '<a class="button" download target="_blank" href="/media/IIIF/'
+                + file
+                + '">'
+            )
+            link += "Download</a>"
+            orphan_data.append(
+                {
+                    "type": "Orphaned IIIF file",
+                    "name": file,
+                    "size": filesizeformat(os.path.getsize(path + file)),
+                    "source": mark_safe(link),
+                }
+            )
+    tables["orphans"] = OrphanTable(orphan_data)
+    tables["orphans"].tab = "#tab-orphans"
     for name, table in tables.items():
         RequestConfig(
-            request, paginate={'per_page': settings.TABLE_ITEMS_PER_PAGE}).configure(table)
-    statvfs = os.statvfs('/var/www/html')
+            request, paginate={"per_page": settings.TABLE_ITEMS_PER_PAGE}
+        ).configure(table)
+    statvfs = os.statvfs("/var/www/html")
     disk_space = statvfs.f_frsize * statvfs.f_blocks
-    free_space = statvfs.f_frsize * statvfs.f_bavail  # available space without reserved blocks
-    disk_space_values = {'total': statvfs.f_frsize * statvfs.f_blocks,
-                         'free': statvfs.f_frsize * statvfs.f_bavail,
-                         'percent': 100 - math.ceil(free_space / (disk_space / 100))}
-    return render(request, 'maps/files/index.html', {
-        'tables': tables, 'disk_space_values': disk_space_values,
-        'orphaned_files_count': orphaned_files_count})
+    free_space = (
+        statvfs.f_frsize * statvfs.f_bavail
+    )  # available space without reserved blocks
+    disk_space_values = {
+        "total": statvfs.f_frsize * statvfs.f_blocks,
+        "free": statvfs.f_frsize * statvfs.f_bavail,
+        "percent": 100 - math.ceil(free_space / (disk_space / 100)),
+    }
+    return render(
+        request,
+        "maps/files/index.html",
+        {
+            "tables": tables,
+            "disk_space_values": disk_space_values,
+            "orphaned_files_count": orphaned_files_count,
+        },
+    )
 
 
 @login_required
 def delete_orphaned_files(request):  # pragma: no cover
     orphaned_files_count = 0
-    path = settings.MEDIA_ROOT + 'scan/'
+    path = settings.MEDIA_ROOT + "scan/"
     scans = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     for file in scans:
-        if not Scan.objects.filter(file='scan/' + file):
+        if not Scan.objects.filter(file="scan/" + file):
             os.remove(os.path.join(path, file))
             orphaned_files_count += 1
-    path = settings.MEDIA_ROOT + 'file/'
+    path = settings.MEDIA_ROOT + "file/"
     files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     for file in files:
-        if not File.objects.filter(file='file/' + file):
+        if not File.objects.filter(file="file/" + file):
             os.remove(os.path.join(path, file))
             orphaned_files_count += 1
-    path = settings.MEDIA_ROOT + 'IIIF/'
+    path = settings.MEDIA_ROOT + "IIIF/"
     files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     for file in files:
         file_name = splitext(basename(file))[0]  # basename without extension
-        if not Scan.objects.filter(file__contains='scan/' + file_name):
+        if not Scan.objects.filter(file__contains="scan/" + file_name):
             os.remove(os.path.join(path, file))
             orphaned_files_count += 1
-    messages.info(request, str(orphaned_files_count) + ' file(s) where deleted.')
-    return redirect('/maps/files')
+    messages.info(request, str(orphaned_files_count) + " file(s) where deleted.")
+    return redirect("/maps/files")
 
 
 @login_required
 def file_detail(request, pk):
     file = File.objects.get(pk=pk)
-    tables = {'maps': MapTable(Map.objects.filter(file_map=file))}
-    tables['maps'].tab = '#maps'
-    tables['references'] = ReferenceTable(Reference.objects.filter(file_reference=file))
-    tables['references'].tab = '#references'
-    return render(request, 'maps/files/file/detail.html', {
-        'file': file, 'tables': tables, 'types': Type.objects.filter(file_type=file)})
+    tables = {"maps": MapTable(Map.objects.filter(file_map=file))}
+    tables["maps"].tab = "#maps"
+    tables["references"] = ReferenceTable(Reference.objects.filter(file_reference=file))
+    tables["references"].tab = "#references"
+    return render(
+        request,
+        "maps/files/file/detail.html",
+        {"file": file, "tables": tables, "types": Type.objects.filter(file_type=file)},
+    )
 
 
 @login_required
 def scan_detail(request, pk):
     scan = Scan.objects.get(pk=pk)
-    tables = {'maps': MapTable(Map.objects.filter(scan_map=scan))}
-    tables['maps'].tab = '#maps'
-    tables['references'] = ReferenceTable(Reference.objects.filter(scan_reference=scan))
-    tables['references'].tab = '#references'
+    tables = {"maps": MapTable(Map.objects.filter(scan_map=scan))}
+    tables["maps"].tab = "#maps"
+    tables["references"] = ReferenceTable(Reference.objects.filter(scan_reference=scan))
+    tables["references"].tab = "#references"
     file_name = splitext(basename(scan.file.name))[0]  # basename without extension
-    iiif = {
-        'file_path': settings.MEDIA_ROOT + 'IIIF/' + file_name,
-        'tile_sources': settings.IIIF_URL + file_name + '/info.json',
-        'library_path': '/static/webpage/libraries/openseadragon/'} if settings.IIIF_URL else None
-    return render(request, 'maps/files/scan/detail.html', {
-        'scan': scan,
-        'iiif': iiif if iiif and os.path.isfile(iiif['file_path'] + '.jp2') else None,
-        'tables': tables,
-        'types': Type.objects.filter(scan_type=scan)})
+    iiif = (
+        {
+            "file_path": settings.MEDIA_ROOT + "IIIF/" + file_name,
+            "tile_sources": settings.IIIF_URL + file_name + "/info.json",
+            "library_path": "/static/webpage/libraries/openseadragon/",
+        }
+        if settings.IIIF_URL
+        else None
+    )
+    return render(
+        request,
+        "maps/files/scan/detail.html",
+        {
+            "scan": scan,
+            "iiif": (
+                iiif if iiif and os.path.isfile(iiif["file_path"] + ".jp2") else None
+            ),
+            "tables": tables,
+            "types": Type.objects.filter(scan_type=scan),
+        },
+    )
 
 
 class FileDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = File
-    success_url = reverse_lazy('maps:files-index')
-    success_message = 'An entry has been deleted.'
+    success_url = reverse_lazy("maps:files-index")
+    success_message = "An entry has been deleted."
 
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, self.success_message)
@@ -167,8 +225,8 @@ class FileDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
 
 class ScanDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Scan
-    success_url = reverse_lazy('maps:files-index')
-    success_message = 'An entry has been deleted.'
+    success_url = reverse_lazy("maps:files-index")
+    success_message = "An entry has been deleted."
 
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, self.success_message)
@@ -177,93 +235,99 @@ class ScanDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
 
 class FileUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = File
-    template_name = 'maps/files/file/update.html'
+    template_name = "maps/files/file/update.html"
     form_class = FileForm
-    success_message = 'An entry has been updated.'
+    success_message = "An entry has been updated."
 
     def post(self, request, **kwargs):
         request.POST = request.POST.copy()
-        request.POST.setlist('file_type', get_selected_nodes('File', request))
+        request.POST.setlist("file_type", get_selected_nodes("File", request))
         return super(FileUpdate, self).post(request, **kwargs)
 
     def dispatch(self, *args, **kwargs):
         return super(FileUpdate, self).dispatch(*args, **kwargs)
 
     def get_success_url(self):
-        return reverse('maps:file-detail', kwargs={'pk': self.object.pk})
+        return reverse("maps:file-detail", kwargs={"pk": self.object.pk})
 
 
 class ScanUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Scan
-    template_name = 'maps/files/scan/update.html'
+    template_name = "maps/files/scan/update.html"
     form_class = ScanForm
-    success_message = 'An entry has been updated.'
+    success_message = "An entry has been updated."
 
     def post(self, request, **kwargs):
         request.POST = request.POST.copy()
-        request.POST.setlist('scan_type', get_selected_nodes('Scan', request))
+        request.POST.setlist("scan_type", get_selected_nodes("Scan", request))
         return super(ScanUpdate, self).post(request, **kwargs)
 
     def dispatch(self, *args, **kwargs):
         return super(ScanUpdate, self).dispatch(*args, **kwargs)
 
     def get_success_url(self):
-        return reverse('maps:scan-detail', kwargs={'pk': self.object.pk})
+        return reverse("maps:scan-detail", kwargs={"pk": self.object.pk})
 
 
 class FileCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = File
-    template_name = 'maps/files/file/create.html'
+    template_name = "maps/files/file/create.html"
     form_class = FileForm
-    success_message = 'An entry has been created.'
+    success_message = "An entry has been created."
 
     def get_form(self):
         form = super(FileCreate, self).get_form(FileForm)
-        if 'origin_id' in self.kwargs and 'class_name' in self.kwargs:
-            if self.kwargs['class_name'] == 'map':
-                form.fields['file_map'].initial = Map.objects.get(pk=self.kwargs['origin_id'])
-            elif self.kwargs['class_name'] == 'reference':
-                form.fields['file_reference'].initial = \
-                    Reference.objects.get(pk=self.kwargs['origin_id'])
+        if "origin_id" in self.kwargs and "class_name" in self.kwargs:
+            if self.kwargs["class_name"] == "map":
+                form.fields["file_map"].initial = Map.objects.get(
+                    pk=self.kwargs["origin_id"]
+                )
+            elif self.kwargs["class_name"] == "reference":
+                form.fields["file_reference"].initial = Reference.objects.get(
+                    pk=self.kwargs["origin_id"]
+                )
         return form
 
     def post(self, request, **kwargs):
         # add types
         request.POST = request.POST.copy()
-        request.POST.setlist('file_type', get_selected_nodes('File', request))
+        request.POST.setlist("file_type", get_selected_nodes("File", request))
         return super(FileCreate, self).post(request, **kwargs)
 
     def dispatch(self, *args, **kwargs):
         return super(FileCreate, self).dispatch(*args, **kwargs)
 
     def get_success_url(self):
-        return reverse('maps:file-detail', kwargs={'pk': self.object.pk})
+        return reverse("maps:file-detail", kwargs={"pk": self.object.pk})
 
 
 class ScanCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Scan
-    template_name = 'maps/files/scan/create.html'
+    template_name = "maps/files/scan/create.html"
     form_class = ScanForm
-    success_message = 'An entry has been created.'
+    success_message = "An entry has been created."
 
     def get_form(self):
         form = super(ScanCreate, self).get_form(ScanForm)
-        if 'origin_id' in self.kwargs and 'class_name' in self.kwargs:
-            if self.kwargs['class_name'] == 'map':
-                form.fields['scan_map'].initial = Map.objects.get(pk=self.kwargs['origin_id'])
-            elif self.kwargs['class_name'] == 'reference':
-                form.fields['scan_reference'].initial = \
-                    Reference.objects.get(pk=self.kwargs['origin_id'])
+        if "origin_id" in self.kwargs and "class_name" in self.kwargs:
+            if self.kwargs["class_name"] == "map":
+                form.fields["scan_map"].initial = Map.objects.get(
+                    pk=self.kwargs["origin_id"]
+                )
+            elif self.kwargs["class_name"] == "reference":
+                form.fields["scan_reference"].initial = Reference.objects.get(
+                    pk=self.kwargs["origin_id"]
+                )
         return form
 
     def post(self, request, **kwargs):
         # Add types
         request.POST = request.POST.copy()
-        request.POST.setlist('scan_type', get_selected_nodes('Scan', request))
+        request.POST.setlist("scan_type", get_selected_nodes("Scan", request))
         return super(ScanCreate, self).post(request, **kwargs)
 
     def dispatch(self, *args, **kwargs):
         return super(ScanCreate, self).dispatch(*args, **kwargs)
 
     def get_success_url(self):
-        return reverse('maps:scan-detail', kwargs={'pk': self.object.pk})
+        return reverse("maps:scan-detail", kwargs={"pk": self.object.pk})
--- maps/views/search.py
+++ maps/views/search.py
@@ -7,39 +7,50 @@ from maps.model.person import Person
 from maps.model.place import Place
 from maps.model.scan import Scan
 from maps.model.reference import Reference
-from maps.tables import InstituteTable, MapTable, PersonTable, PlaceTable, ReferenceTable, ScanTable
+from maps.tables import (
+    InstituteTable,
+    MapTable,
+    PersonTable,
+    PlaceTable,
+    ReferenceTable,
+    ScanTable,
+)
 
 
 @login_required
 def index(request):
-    search_term = request.POST.get('search-term')
+    search_term = request.POST.get("search-term")
     if not search_term:
-        return render(request, 'maps/search/index.html')
+        return render(request, "maps/search/index.html")
     result_tables = {}
-    maps = Map.objects.filter(name__icontains=search_term) or \
-           Map.objects.filter(title__icontains=search_term)
+    maps = Map.objects.filter(name__icontains=search_term) or Map.objects.filter(
+        title__icontains=search_term
+    )
     if maps:
         table = MapTable(maps)
-        result_tables['Maps'] = table
+        result_tables["Maps"] = table
     references = Reference.objects.filter(name__icontains=search_term)
     if references:
         table = ReferenceTable(references)
-        result_tables['References'] = table
+        result_tables["References"] = table
     persons = Person.objects.filter(name__icontains=search_term)
     if persons:
         table = PersonTable(persons)
-        result_tables['Persons'] = table
+        result_tables["Persons"] = table
     institutes = Institute.objects.filter(name__icontains=search_term)
     if institutes:
         table = InstituteTable(institutes)
-        result_tables['Institutes'] = table
+        result_tables["Institutes"] = table
     places = Place.objects.filter(name__icontains=search_term)
     if places:
         table = PlaceTable(places)
-        result_tables['Places'] = table
+        result_tables["Places"] = table
     scans = Scan.objects.filter(name__icontains=search_term)
     if scans:
         table = ScanTable(scans)
-        result_tables['Scans'] = table
-    return render(request, 'maps/search/index.html',
-                  {'search_term': search_term, 'result_tables': result_tables})
+        result_tables["Scans"] = table
+    return render(
+        request,
+        "maps/search/index.html",
+        {"search_term": search_term, "result_tables": result_tables},
+    )
